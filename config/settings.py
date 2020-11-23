76265c6d46c79fee92c65c344bfefe1a76333b5b
Michael Kranker <me@michaelkranker.com>
2020-11-22 20:35:36 -0500

    secret_key moved to .env

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qzo)=6h!o6d-s4!)p(tc^rfrb^n*5-rfgaan0n67*p5-(m!$m&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
